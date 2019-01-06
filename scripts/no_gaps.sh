22dd85c8d0fd7a4b9ddc0a23a94a18a98f17d841
Henning <henningg1202@gmail.com>
2019-01-06 18:56:58 +0100

    pre-dt-polybar

--- scripts/no_gaps.sh
+++ scripts/no_gaps.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 i3-msg gaps inner all set 0
 i3-msg gaps outer all set 0
-i3-msg gaps top all set 25
-i3-msg gaps bottom all set 25
+i3-msg gaps bottom all set 0
--- scripts/restarti3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-i3-msg restart
-/home/henning/scripts/repbar.sh
