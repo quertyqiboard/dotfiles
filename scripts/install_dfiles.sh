6eaf726942d35719669604881f129c54267e9b0f
Henning <henningg1202@gmail.com>
2019-01-13 18:28:00 +0100

    Fixed Xres and vimrc installation

--- scripts/install_dfiles.sh
+++ scripts/install_dfiles.sh
@@ -12,10 +12,8 @@ mv ~/dotfiles/dotfiles/ /tmp/dotfiles
 mv ~/dotfiles/backgrounds ~/backgrounds
 mv -n ~/dotfiles/.config ~/.config
 mv ~/dotfiles/* ~/
-mv /tmp/dotfiles/* ~/dotfiles
-ln -s ~/dotfiles/Xresources ~/.Xresources
+mv /tmp/dotfiles/* ~/dotfiles/
 ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash_history ~/.bash_history
 ln -s ~/dotfiles/i3config ~/.config/i3/config
 ln -s ~/dotfiles/xinitrc ~/.xinitrc
