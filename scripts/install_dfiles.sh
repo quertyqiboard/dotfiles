3acd5d9480ca7a5471f48365ac7cd7ef7dcf13f0
Henning <henningg1202@gmail.com>
2019-01-13 18:13:48 +0100

    Seperated program installation

--- scripts/install_dfiles.sh
+++ scripts/install_dfiles.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Install nerd fonts (optional)
-# git clone https://github.com/ryanoasis/nerd-fonts.git
-# ~/nerd-fonts/install.sh
-# Install necessary programs for workflow
-pacman -S nitrogen rofi xorg-xbacklight arandr pcmanfm playerctl
 # Remove existing dotfiles
 rm -f ~/.Xresources || unlink ~/.Xresources
 rm -f ~/.bashrc || unlink ~/.bashrc
---
+++ scripts/programs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install nerd fonts (optional)
+# git clone https://github.com/ryanoasis/nerd-fonts.git
+# ~/nerd-fonts/install.sh
+# Install necessary programs for workflow
+pacman -S nitrogen rofi xorg-xbacklight arandr pcmanfm playerctl
