c2941bf9ddcc78d74099f1b20231575605fbdf43
Henning <henningg1202@gmail.com>
2019-01-13 18:44:02 +0100

    Bad debugging in inst scr

--- scripts/install_dfiles.sh
+++ scripts/install_dfiles.sh
@@ -9,11 +9,17 @@ rm -f ~/.config/i3/config || unlink ~/.config/i3/config
 rm -f ~/.xinitrc || unlink ~/.xinitrc
 # Move the folders to the correct position
 mv ~/dotfiles/scripts ~/scripts
-mv ~/dotfiles/dotfiles/ /tmp/dotfiles
+echo "1 $(ls ~/dotfiles)"
+mv ~/dotfiles/dotfiles /tmp/dotfiles
+echo "2 $(ls ~/dotfiles)"
 mv ~/dotfiles/backgrounds ~/backgrounds
+echo "3 $(ls ~/dotfiles)"
 mv -n ~/dotfiles/.config ~/.config
+echo "4 $(ls ~/dotfiles)"
 mv ~/dotfiles/* ~/
+echo "5 $(ls ~/dotfiles)"
 mv /tmp/dotfiles/* ~/dotfiles/
+echo "6 $(ls ~/dotfiles)"
 # Vim setup
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/src
