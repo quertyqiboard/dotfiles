5ccac1c156383f609b1b01d6c35cf7a10eb6fdc3
Henning <henningg1202@gmail.com>
2019-03-11 12:52:15 +0100

    Changed gaps size

--- scripts/dmenu/shutdown.sh
+++ scripts/dmenu/shutdown.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-choices="Cancel\nReboot\nShutdown\nLock screen\nExit i3\nTake screenshot\nReset i3"
+choices="Cancel\nReboot\nOff\nLock screen\nExit i3\nTake screenshot\nReset i3"
 chosen=$(echo -e "$choices" | dmenu -i)
 case $chosen in
-	"Shutdown") poweroff;;
+	"Off") poweroff;;
 	"Reboot") reboot;;
 	"Lock screen") i3lock -B 8;;
 	"Exit i3") i3exit logout ;;
 	"Take screenshot") scrot;;
-	# "Max i3gaps" ) ~/scripts/i3gaps/gaps_max.sh ;;
-	# "Min i3gaps" )  ~/scripts/i3gaps/zero.sh;;
-	"Reset i3" ) ~/scripts/i3gaps/gaps_max.sh;;
+	"Reset 3" ) ~/scripts/i3gaps/gaps_max.sh;;
 esac
 
