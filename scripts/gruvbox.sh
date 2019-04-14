8d4d90bbcedef234d3780c4f0a6ce0b8f8c8f55e
Henning <henningg1202@gmail.com>
2019-04-14 18:51:52 +0200

    Switched to neovim

--- scripts/gruvbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# update vimrc
-cat /home/henning/dotfiles/vimrc > /home/henning/.vimrc
-cat /home/henning/scripts/colorschemes/cs_transparent >> /home/henning/.vimrc
-# Update xresources
-cat /home/henning/dotfiles/Xresources > /home/henning/.Xresources
-cat /home/henning/scripts/colorschemes/gruvbox >> /home/henning/.Xresources
-# reload xresources
-xrdb -load /home/henning/.Xresources
--- scripts/schedule.py
+++ scripts/schedule.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3.7
 import datetime
-day1a,day2a ,day3a ,day4a ,day5a ,day6a, day7a  = "Che  Spo  MuK  The  The","Ges  Inf  Mat  Spa","Bio  Mat  Deu  Spo","MuK  Deu  Eng","Phy  Pol  Spa","Wochende","Wochende"
-day1b,day2b,day3b,day4b,day5b,day6b,day7b  = "Bio  Inf  MuK","Phy  Eng  Mat  Spa","Eng  Che  Deu  Spo","Nix  Deu  Ges  Team","Pol  The  Mat", "Wochenende", "Wochenende"
-wa = [day1a, day2a, day3a, day4a, day5a, day6a, day7a,]
-wb = [day1b, day2b, day3b, day4b, day5b, day6b, day7b,]
+day1a, day2a, day3a, day4a, day5a, day6a, day7a = "Che  Spo  MuK  The  The", "Ges  Inf  Mat  Spa", "Bio  Mat  Deu  Spo", "MuK  Deu  Eng", "Phy  Pol  Spa", "Wochende", "Wochende"
+day1b, day2b, day3b, day4b, day5b, day6b, day7b = "Bio  Inf  MuK", "Phy  Eng  Mat  Spa", "Eng  Che  Deu  Spo", "Nix  Deu  Ges  Team", "Pol  The  Mat", "Wochenende", "Wochenende"
+WA = [
+    day1a,
+    day2a,
+    day3a,
+    day4a,
+    day5a,
+    day6a,
+    day7a,
+]
+wb = [
+    day1b,
+    day2b,
+    day3b,
+    day4b,
+    day5b,
+    day6b,
+    day7b,
+]
 wd = datetime.datetime.today().weekday()
 wk = datetime.datetime.today().isocalendar()[1]
 if wk % 2 == 0:
     if wd == 6:
-        output=wa[0]
+        output = wa[0]
     else:
-        output=wb[wd+1]
+        output = wb[wd + 1]
     output += " (B)"
 else:
     if wd == 6:
-        output=wb[0]
+        output = wb[0]
     else:
-        output=wa[wd+1]
+        output = wa[wd + 1]
     output += " (A)"
 print(output)
